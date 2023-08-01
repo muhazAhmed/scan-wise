ba3bd468b4c69feb478c536d4aa582e1c86d54c4
Muhaz Ahmed <muhazvla313@gmail.com>
2023-08-01 18:00:48 +0530

    added route for admin

--- server/src/route.js
+++ server/src/route.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const adminController = require ("./controllers/adminController")
 
+// ============= Testing ============
 router.get("/", (req, res) => {
   return res.json("Api is Working!...");
 });
+
+// ============= Admin ================
+router.post("/api/user/register", adminController.register)
+router.post("/api/user/login", adminController.login)
+router.delete("/api/user/:id", adminController.deleteAdmin)
 
 module.exports = router;
