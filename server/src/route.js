7ad51be28f326060838f066c6dfb7e93aaffb7b3
Muhaz Ahmed <muhazvla313@gmail.com>
2023-08-01 19:16:37 +0530

    added routes for employee

--- server/src/route.js
+++ server/src/route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require ("./controllers/adminController")
+const employeeController = require("./controllers/employeeController")
 
 // ============= Testing ============
 router.get("/", (req, res) => {
@@ -11,5 +12,13 @@ router.get("/", (req, res) => {
 router.post("/api/user/register", adminController.register)
 router.post("/api/user/login", adminController.login)
 router.delete("/api/user/:id", adminController.deleteAdmin)
+router.patch("/api/user/:id", adminController.updateAdmin)
+router.post("/api/user/:id", adminController.logout)
+
+// ============= Employee ==============
+router.post("/api/emp/register", employeeController.register)
+router.post("/api/emp/login", employeeController.login)
+router.delete("/api/emp/:id", employeeController.deleteEmp)
+router.post("/api/emp/:id", employeeController.logout)
 
 module.exports = router;
