068d67f549fa943f368a38077eddb39fdd33867d
Muhaz Ahmed <muhazvla313@gmail.com>
2023-08-11 11:30:49 +0530

    added suspence, loader

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { AuthContextProvider } from "./utils/AuthContext.jsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = createTheme();
+
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthContextProvider>
+        <App />
+        <Toaster position="top-right" />
+      </AuthContextProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
